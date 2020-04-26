f6c33a0ef7f1b2ffd346fcf477bae1dc61e24b10
lyle-poli <58298534+lyle-poli@users.noreply.github.com>
2020-04-27 00:30:34 +0200

    Started work on basic link budget script
    Started working on the basics for link budget between devices, accurate numbers achieved thus far

--- system_link_budgets.m
+++ system_link_budgets.m
@@ -1,5 +1,31 @@
+%Later this script will get turned into a function called by
+%system_characteristics
+clearvars
+clc
 %% Earth - Mars ECS
-%[] = link_budget();
+%Shared properties
+frq = 34.5e9;   %[Hz] Signal nominal frequency, 8 GHz for MRO
+BW = 1.75e8;     %[Hz] nominal bandwidth
+dist = 400e9;  %[m] Distance between transmitter and receiver
+%design margin
+
+%Transmitter properties
+pow_t = 2e5;   %[W] Signal power sent to antenna, 20kW for X band DSN
+dia_t = 34;    %[m] Diameter of transmitting dish
+e_a = 0.94;   %[-] Aperture efficiency of transmitting antenna
+atm_t = 0.9;    %[-] Transmission coefficient through atmosphere (EARTH)
+%rain fade
+
+%Receiver properties
+dia_r = 4.5;    %[m] Diameter of receiving dish
+k_r = 0.8;      %[-] Aperture efficiency of receiving dish
+
+Ear_ECS.name = 'Earth to Mars ECS Uplink Budget';
+[Ear_ECS.budget] = zfun_link_budget(frq,BW,dist,pow_t,dia_t,e_a,atm_t, ...
+    dia_r,k_r);
+
+disp(Ear_ECS.name)
+disp(Ear_ECS.budget)
 
 %% Mars ECS - Mars RS
 % Call the link budget function
--- zfun_link_budget.m
+++ zfun_link_budget.m
@@ -1,5 +1,28 @@
-function [] = link_budget()
+function [output] = zfun_link_budget(frq,BW,dist,pow_t,dia_t,e_a,atm_t, ...
+    dia_r,k_r)
 %This function will take input variables about the link in question and
 %populate the nominal link budget.
 %This code should be compared with a couple of online calculators and
 %reference papers such as MRO to ensure it is correct.
+
+lambda = physconst('LightSpeed')/frq;   %[m] wavelength of signal
+
+PdB_t = 10*log10(pow_t);      %[dBW] Transmitted signal power
+G_t   = e_a*(pi*dia_t/lambda)^2;     %[-] Transmitting antenna gain
+GdB_t = 10*log10(G_t);        %[dBi] Gain relative to isotropic transmitter
+
+pathdB_L = -20*log10(4*pi*dist/lambda);  %[dB] Propagation Loss
+
+G_r = k_r*(pi*dia_r/lambda)^2;       %[-] Receiving antenna gain
+GdB_r = 10*log10(G_r);        %[dBi] Gain relative to isotropic receiver
+PdB_r = PdB_t + GdB_t + pathdB_L + GdB_r;   %[dBW] Received signal power
+%Noise temperature etc.  %physconst('Boltzmann'); 
+
+%Saving variables to an output structure for use in calling script
+output.PdB_t = PdB_t;
+output.GdB_t = GdB_t;
+output.pathdB_L = pathdB_L;
+output.GdB_r = GdB_r;
+output.PdB_r = PdB_r;
+
+
