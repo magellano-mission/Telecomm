5ce61904f6c54ad56785caa0dfa5bdc3c9118793
lyle-poli <58298534+lyle-poli@users.noreply.github.com>
2020-04-26 18:33:37 +0200

    Templates for evaluating the link budgets required
    Created simple templates for the link budget function and the combined system analysis file

---
+++ system_link_budgets.m
@@ -0,0 +1,21 @@
+%% Earth - Mars ECS
+%[] = link_budget();
+
+%% Mars ECS - Mars RS
+% Call the link budget function
+
+%% Mars RS - Mars RS
+% Call the link budget function
+
+%% Mars RS - Mars Surface User
+% Call the link budget function
+
+%% Mars RS - Mars Orbital User
+% Call the link budget function
+
+%Later add off-nominal cases when Earth or ECS talk directly to Users in
+%orbit or on surface. Later can consider if we want to add additional links
+%in the chain back to Earth or around Mars
+
+
+
---
+++ zfun_link_budget.m
@@ -0,0 +1,5 @@
+function [] = link_budget()
+%This function will take input variables about the link in question and
+%populate the nominal link budget.
+%This code should be compared with a couple of online calculators and
+%reference papers such as MRO to ensure it is correct.
