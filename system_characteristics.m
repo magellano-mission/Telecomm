3fd9a642e23dbaf1b6ded142f58930a0ee737099
lyle-poli <58298534+lyle-poli@users.noreply.github.com>
2020-04-27 16:40:03 +0200

    Added some spreadheets
    Added a spreadsheet with a sample link budget for Earth-Mars and Mars-Mars links. Also added a House of Quality spreadsheet for telecoms

--- system_characteristics.m
+++ system_characteristics.m
@@ -4,3 +4,6 @@
 
 % Perhaps we can run some kind of optimising function to optimise a cost
 % function involving things like data rates and coverage / connectivity
+
+%create a separate function for the navigation aspect then we can do a
+%trade-off between these options
